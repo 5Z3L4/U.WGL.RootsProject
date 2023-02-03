3e21ce197fd0120692d38a91b987f00e09261e50
Adrian Barczewski <adrianbarczewski@gmail.com>
2023-02-03 22:07:19 +0100

    water fixed

--- Assets/_Project/Scripts/WaterMovement.cs
+++ Assets/_Project/Scripts/WaterMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WaterMovement : MonoBehaviour
 {
-    void Update()
+    private void Update()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
