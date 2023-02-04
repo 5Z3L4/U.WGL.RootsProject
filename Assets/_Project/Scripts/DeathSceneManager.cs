61ecee6854ea3f86b57d127937c206e1eb605f06
Matitek <mpawelczak87@gmail.com>
2023-02-04 12:42:17 +0100

    fixed ui on death scene

--- Assets/_Project/Scripts/DeathSceneManager.cs
+++ Assets/_Project/Scripts/DeathSceneManager.cs
@@ -9,11 +9,13 @@ public class DeathSceneManager : MonoBehaviour
     [SerializeField] private GameObject _deathScene;
     [SerializeField] private TMP_Text _playerScore;
     [SerializeField] private Button _playAgainButton;
+    [SerializeField] private Button _mainMenuButton;
 
     private void Start()
     {
         _deathScene.SetActive(false);
         _playAgainButton.onClick.AddListener(LoadScene);
+        _mainMenuButton.onClick.AddListener(LoadMainMenu);
     }
 
     public void DisplayDeathScene()
@@ -24,5 +26,10 @@ public class DeathSceneManager : MonoBehaviour
     private void LoadScene()
     {
         SceneManager.LoadScene(_sceneName);
+    }
+
+    private void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
