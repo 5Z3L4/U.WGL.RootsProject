8ee8d1d601fe991424c6bac733123cc4736174be
Adrian Barczewski <adrianbarczewski@gmail.com>
2023-02-05 15:09:31 +0100

    Skeletons added

--- Assets/_Project/Scripts/EnemiesController.cs
+++ Assets/_Project/Scripts/EnemiesController.cs
@@ -6,10 +6,15 @@ public class EnemiesController : MonoBehaviour
     public static EnemiesController Instance { get; private set; }
     [SerializeField] private CanvasManager _canvasManager;
     [SerializeField] private GameObject _enemyToSpawn;
+    [SerializeField] private GameObject _skeletonEnemyToSpawn;
+
     public Transform PlayerTransform;
     [SerializeField] private float _speed;
+    [SerializeField] private float _skeletonSpeed;
 
     [SerializeField] private List<Transform> _respawnPoints;
+    [SerializeField] private List<Transform> _skeletonRespawnPoints;
+
     [SerializeField] private float _respawnTime;
     [SerializeField] private float _respawnTimeOnStart;
     private float _playTime;
@@ -43,6 +48,9 @@ public class EnemiesController : MonoBehaviour
             int pointNumber = Random.Range(0, _respawnPoints.Count);
             GameObject enemy = Instantiate(_enemyToSpawn, _respawnPoints[pointNumber]);
             enemy.GetComponent<Enemy>().SetEnemyData(this, _canvasManager, PlayerTransform, _speed);
+
+            GameObject skeletonEnemy = Instantiate(_skeletonEnemyToSpawn, _respawnPoints[pointNumber]);
+            skeletonEnemy.GetComponent<SkeletonEnemy>().SetEnemyData(this, _canvasManager, PlayerTransform, _skeletonSpeed);
         }
 
         if (_playTime >= 8)
