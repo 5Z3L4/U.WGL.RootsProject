1492970a4d4ccbdd47ce876c56ef1a9c0592d562
Matitek <mpawelczak87@gmail.com>
2023-02-05 15:15:16 +0100

    added credits

--- Assets/_Project/Scripts/MenuManager.cs
+++ Assets/_Project/Scripts/MenuManager.cs
@@ -1,4 +1,3 @@
-using TMPro;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -19,10 +18,6 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject _tutorialPanel;
     [SerializeField] private GameObject _optionsPanel;
     [SerializeField] private GameObject _creditsPanel;
-    [SerializeField] private TMP_Text _creditsText;
-    [Space(10)]
-    [TextArea(15, 20)]
-    [SerializeField] private string _credits;
 
     void Start()
     {
@@ -36,8 +31,6 @@ public class MenuManager : MonoBehaviour
         _tutorialPanel.SetActive(true);
         _tutorialPanel.transform.localScale = Vector3.zero;
 
-        _creditsText.text = _credits;
-
         _playButton.onClick.AddListener(() => LoadScene("Water"));
         _startTutorialButton.onClick.AddListener(() => OpenPanel(_tutorialPanel));
         _optionsButton.onClick.AddListener(() => OpenPanel(_optionsPanel));
@@ -45,7 +38,6 @@ public class MenuManager : MonoBehaviour
         _returnOptionsButton.onClick.AddListener(() => HidePanel(_optionsPanel));
         _returnCreditsButton.onClick.AddListener(() => HidePanel(_creditsPanel));
         _returnFromTutorialToMenuButton.onClick.AddListener(() => HidePanel(_tutorialPanel));
-
     }
 
     private void LoadScene(string sceneName)
